12290b0df2a51dd3fe749581cc2a7f71a4424029
opcitizen <goblinkun98@gmail.com>
2025-12-22 12:10:12 +0545

    states of form

--- my-react-app/src/Routes.jsx
+++ my-react-app/src/Routes.jsx
@@ -3,15 +3,19 @@ import {BrowserRouter,Route,Routes} from "react-router-dom"
 import About from './pages/About'
 import Contacts from './pages/Contacts'
 import Home from './pages/Home'
+import Layout from './Layout'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
     
     <Routes>
+          <Route path="/" element={<Layout />}>
         <Route path='/about' element={<About />} />
-        <Route path='/contacts' element={<Contacts/>}/>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contacts/>}/>
+          <Route index element={<Home />} />
+           </Route>
+
 
         
     </Routes>
--- my-react-app/src/pages/Contacts.jsx
+++ my-react-app/src/pages/Contacts.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import './Contact.css'
 export default function Contacts() {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [message,setMessage] = useState("")
+
+
+  const handleSubmission = (e)=>{
+    e.preventDefault();
+    const formData = {
+      name : name,
+      email : email,
+      message : message
+    };
+    console.log("Form Data Submitted : ",formData)
+  }
   return (
-  <h1>Contacts</h1>
-  )
+    <form className="form" onSubmit={handleSubmission}>
+
+      <div className="form-group">
+        <label htmlFor="name">Name: </label>
+        <input type="text" name="name" onChange={(e)=>setName(e.target.value)}/>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Email: </label>
+        <input type="email" name="email" onChange={(e)=>setEmail(e.target.value)}/>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="message">Message: </label>
+        <textarea name="message" rows="5" onChange={(e)=>setMessage(e.target.value)}></textarea>
+        <button type="submit">Send</button>
+      </div>
+
+    </form>
+  );
 }
