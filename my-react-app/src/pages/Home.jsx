2d6332d91a9716ba6542623fb1ab4191342b517b
opcitizen <goblinkun98@gmail.com>
2026-01-13 12:04:32 +0545

    theme toggle

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CustomButton from '../components/CustomButton/CustomButton'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 export default function Home() {
+  const {theme,setTheme} = useContext(ThemeContext);
+  console.log("current theme",theme)
+  const handleToggle = ()=>{
+    setTheme(theme === "light"?"dark":"light");
+  }
   return (
     <>
- <CustomButton type={'primary'}/>
- <CustomButton type={'outline'}/>
+    <p>current theme is {theme}</p>
+  <button onClick={handleToggle}>Toggle theme</button>
     </>
 
   )
